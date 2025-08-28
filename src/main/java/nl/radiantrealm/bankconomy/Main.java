435b6e036eea2a5b60efb4c6c90c6faa728969ce
Josje <paul.verbael@gmail.com>
2025-08-29 00:31:21 +0200

    Moved cache initialization from Cache to Main
    Moved cache initialization from Cache.java to Main.java and expanded to include SavingsAccountCache and SavingsOwnerCache. Deleted the now redundant Cache.java to simplify cache management.

--- src/main/java/nl/radiantrealm/bankconomy/Main.java
+++ src/main/java/nl/radiantrealm/bankconomy/Main.java
@@ -1,11 +1,14 @@
 package nl.radiantrealm.bankconomy;
 
-import nl.radiantrealm.bankconomy.controller.Cache;
-import nl.radiantrealm.bankconomy.network.InternalNetworkAPI;
+import nl.radiantrealm.bankconomy.cache.PlayerAccountCache;
+import nl.radiantrealm.bankconomy.cache.SavingsAccountCache;
+import nl.radiantrealm.bankconomy.cache.SavingsOwnerCache;
 
 public class Main {
+    public static final PlayerAccountCache playerAccountCache = new PlayerAccountCache();
+    public static final SavingsAccountCache savingsAccountCache = new SavingsAccountCache();
+    public static final SavingsOwnerCache savingsOwnerCache = new SavingsOwnerCache();
+
     public static void main(String[] args) {
-        new Cache();
-        new InternalNetworkAPI();
     }
 }
