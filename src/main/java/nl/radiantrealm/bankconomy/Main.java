466cc9a4a17858112e984128d6fcd17e567bc338
Josje <paul.verbael@gmail.com>
2025-09-03 22:07:16 +0200

    Update Main.java

--- src/main/java/nl/radiantrealm/bankconomy/Main.java
+++ src/main/java/nl/radiantrealm/bankconomy/Main.java
@@ -3,13 +3,11 @@ package nl.radiantrealm.bankconomy;
 import nl.radiantrealm.bankconomy.cache.GovernmentFundsCache;
 import nl.radiantrealm.bankconomy.cache.PlayerAccountCache;
 import nl.radiantrealm.bankconomy.cache.SavingsAccountCache;
-import nl.radiantrealm.bankconomy.cache.SavingsOwnerCache;
 
 public class Main {
     public static final GovernmentFundsCache governmentFundsCache = new GovernmentFundsCache();
     public static final PlayerAccountCache playerAccountCache = new PlayerAccountCache();
     public static final SavingsAccountCache savingsAccountCache = new SavingsAccountCache();
-    public static final SavingsOwnerCache savingsOwnerCache = new SavingsOwnerCache();
 
     public static void main(String[] args) {
     }
