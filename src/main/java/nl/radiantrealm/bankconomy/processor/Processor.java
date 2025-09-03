874eb31bb5ec609c770f996eeb3de2665899ebc1
Josje <paul.verbael@gmail.com>
2025-09-03 18:52:28 +0200

    Refactor Processor to use ProcessHandler
    Replaces ProcessType with ProcessHandler in process creation and updates method visibility and logger initialization. Improves error handling by using error codes and restricts handleNextProcess to private.

--- src/main/java/nl/radiantrealm/bankconomy/processor/Processor.java
+++ src/main/java/nl/radiantrealm/bankconomy/processor/Processor.java
@@ -3,6 +3,7 @@ package nl.radiantrealm.bankconomy.processor;
 import com.google.gson.JsonObject;
 import nl.radiantrealm.library.ApplicationService;
 import nl.radiantrealm.library.processor.Process;
+import nl.radiantrealm.library.processor.ProcessHandler;
 import nl.radiantrealm.library.processor.ProcessResult;
 import nl.radiantrealm.library.utils.Logger;
 
@@ -13,7 +14,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Consumer;
 
 public class Processor implements ApplicationService {
-    private final Logger logger = Logger.getLogger(Processor.class);
+    private final Logger logger = Logger.getLogger(Process.class);
 
     private static final AtomicInteger processID = new AtomicInteger(0);
     private static final Map<Integer, Process> processMap = new ConcurrentHashMap<>();
@@ -37,7 +38,7 @@ public class Processor implements ApplicationService {
         task.cancel(false);
     }
 
-    protected void handleNextProcess() {
+    private void handleNextProcess() {
         if (processMap.isEmpty()) return;
 
         int nextProcessID = Collections.min(processMap.keySet());
@@ -48,17 +49,17 @@ public class Processor implements ApplicationService {
             process.consumer().accept(result);
         } catch (Exception e) {
             String error = String.format("Unexpected error in %s whilst processing request.", process.handler().getClass().getSimpleName());
-            process.consumer().accept(ProcessResult.error(error, e));
+            process.consumer().accept(ProcessResult.error(500, error));
             logger.error(error, e);
         }
     }
 
-    public static void createProcess(ProcessType processType, JsonObject object, Consumer<ProcessResult> consumer) throws IllegalArgumentException {
+    public static void createProcess(ProcessHandler handler, JsonObject object, Consumer<ProcessResult> consumer) throws IllegalArgumentException {
         int nextProcessID = processID.incrementAndGet();
 
         processMap.put(nextProcessID, new Process(
                 nextProcessID,
-                processType.handler,
+                handler,
                 object,
                 consumer
         ));
